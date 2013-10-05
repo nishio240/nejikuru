38c234097ff7ff36f1ba506435a5c2396087122d
tsuchiyahiroshi <h.tsuchiya@prographic.jp>
2013-10-05 18:20:50 +0900

    H.Tsuchiya

--- define.php
+++ define.php
@@ -1,15 +1,15 @@
 <?php
-/** HTMLディレクトリからのDATAディレクトリの相対パス */
+/** HTML繝�ぅ繝ｬ繧ｯ繝医Μ縺九ｉ縺ｮDATA繝�ぅ繝ｬ繧ｯ繝医Μ縺ｮ逶ｸ蟇ｾ繝代せ */
 define('HTML2DATA_DIR', '/data/');
 
-/** data/module 以下の PEAR ライブラリを優先的に使用する */
+/** 9data/module 莉･荳九� PEAR 繝ｩ繧､繝悶Λ繝ｪ繧貞━蜈育噪縺ｫ菴ｿ逕ｨ縺吶ｋ */
 set_include_path(realpath(dirname(__FILE__) . '/' . HTML2DATA_DIR . 'module') . PATH_SEPARATOR . get_include_path());
 
 /**
- * DIR_INDEX_FILE にアクセスするときにファイル名を使用するか
+ * DIR_INDEX_FILE 縺ｫ繧｢繧ｯ繧ｻ繧ｹ縺吶ｋ縺ｨ縺阪↓繝輔ぃ繧､繝ｫ蜷阪ｒ菴ｿ逕ｨ縺吶ｋ縺�
  *
- * true: 使用する, false: 使用しない, null: 自動(IIS は true、それ以外は false)
- * ※ IIS は、POST 時にファイル名を使用しないと不具合が発生する。(http://support.microsoft.com/kb/247536/ja)
+ * true: 菴ｿ逕ｨ縺吶ｋ, false: 菴ｿ逕ｨ縺励↑縺� null: 閾ｪ蜍�IIS 縺ｯ true縲√◎繧御ｻ･螟悶� false)
+ * 窶ｻ IIS 縺ｯ縲￣OST 譎ゅ↓繝輔ぃ繧､繝ｫ蜷阪ｒ菴ｿ逕ｨ縺励↑縺�→荳榊�蜷医′逋ｺ逕溘☆繧九�(http://support.microsoft.com/kb/247536/ja)
  */
 define('USE_FILENAME_DIR_INDEX', null);
 
